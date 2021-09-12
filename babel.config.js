0927310aa3bef29e87121f5ba3ad4b21f7d05884
b10cker <x.wu3@uqconnect.edu.au>
2021-09-12 16:45:54 +0800

    feat: Add Vant UI library into project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  plugins: [
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
+  ],
   presets: ["@vue/cli-plugin-babel/preset"],
 };
