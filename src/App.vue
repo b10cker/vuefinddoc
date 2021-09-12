6c0254e790fa8b13b8a664b1d593f9ce39cfecad
b10cker <x.wu3@uqconnect.edu.au>
2021-09-12 22:05:02 +0800

    Add vueg, add transition

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!--      <router-link to="/">Home</router-link> |-->
-      <!--      <router-link to="/about">About</router-link> |-->
-    </div>
-    <!--    <router-link to="/create">-->
-    <!--    </router-link>-->
     <keep-alive>
-      <router-view />
+      <router-view class="router-view-class" v-transition />
     </keep-alive>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import vueg from "vueg";
+import router from "./router";
+const options = {
+  duration: "0.3", //转场动画时长，默认为0.3，单位秒
+  firstEntryDisable: false, //值为true时禁用首次进入应用时的渐现动画，默认为false
+  firstEntryDuration: ".6", //首次进入应用时的渐现动画时长，默认为.6
+  forwardAnim: "fadeInRight", //前进动画，默认为fadeInRight
+  backAnim: "fadeInLeft", //后退动画，默认为fedeInLeft
+  sameDepthDisable: false, //url深度相同时禁用动画，默认为false
+};
+
+Vue.use(vueg, router, options);
+
 export default {
   methods: {
     toSignUp() {
@@ -23,6 +31,10 @@ export default {
 </script>
 
 <style lang="less">
+.router-view-class {
+  background-color: white;
+  min-height: 100vh;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
