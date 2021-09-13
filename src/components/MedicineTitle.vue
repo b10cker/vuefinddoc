dd80820f39c9db8f77f041d2c0603e48b395c6f7
b10cker <x.wu3@uqconnect.edu.au>
2021-09-13 17:10:14 +0800

    Add: MedicineTitle componet

---
+++ src/components/MedicineTitle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="title-container">
+    <div class="goBack-icon">
+      <i class="fas fa-arrow-left fa-2x" @click="function(){this.$router.go(-1)}"></i>
+    </div>
+    <h2>{{ titleText }}</h2>
+    <div class="empty"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleText: [String],
+  },
+};
+</script>
+
+<style scoped>
+.title-container {
+  padding: 0 10px;
+  background-color: #396cf0;
+  height: 130px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.title-container div {
+  width: 50px;
+}
+.goBack-icon {
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+}
+h2 {
+  color: white;
+  display: flex;
+  justify-content: flex-start;
+}
+</style>
