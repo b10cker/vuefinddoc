cd503f28fbe770c79375e291bcbefe630b2baca5
b10cker <x.wu3@uqconnect.edu.au>
2021-09-22 23:15:47 +0800

    feat: Add vars

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    username: "",
+    password: "",
+    email: "",
+    age: "",
+    gender: "",
+    languagePreference: "",
+  },
   mutations: {},
   actions: {},
   modules: {},
